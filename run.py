afcec5952ead7e527840a80e8bcbf19c0c24726b
alpha33tt <benwilsonj087@gmail.com>
2025-03-26 02:30:33 -0700

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
+import os
 from app import create_app
-from app.tasks import celery
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
